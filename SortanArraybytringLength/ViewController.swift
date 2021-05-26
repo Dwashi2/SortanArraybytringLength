0dced74c257f66174304ef979fa6fc6a38f560e9
Daniel Washington <danielvertigo@hotmail.com>
2021-05-26 16:37:34 -0300

    Files

--- SortanArraybytringLength/ViewController.swift
+++ SortanArraybytringLength/ViewController.swift
@@ -5,6 +5,23 @@
 //  Created by Daniel Washington Ignacio on 26/05/21.
 //
 
+
+/*
+ Create a function that takes an array of strings and return an array, sorted from shortest to longest.
+
+ Examples
+
+ sortByLength(["Google", "Apple", "Microsoft"])
+ ➞ ["Apple", "Google", "Microsoft"]
+
+ sortByLength(["Leonardo", "Michelangelo", "Raphael", "Donatello"])
+ ➞ ["Raphael", "Leonardo", "Donatello", "Michelangelo"]
+
+ sortByLength(["Turing", "Einstein", "Jung"])
+ ➞ ["Jung", "Turing", "Einstein"]
+ */
+
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,8 +29,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //print(sortByLength(["Google", "Apple", "Microsoft"]))
+        //print(sortByLength(["Turing", "Einstein", "Jung"]))
+        print(sortByLength(["Leonardo", "Michelangelo", "Raphael", "Donatello"]))
     }
 
+    
+    func sortByLength(_ arr: [String]) -> [String] {
+        return arr.sorted{$0.count < $1.count}
+    }
 
 }
 
